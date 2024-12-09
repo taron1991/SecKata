f942ba26d24de42455c5e8af776abb05da9a8c8b
Taron Gukasyan <funkadelica@mail.ru>
2024-12-09 14:59:32 +0300

    h6

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleService.java
@@ -1,23 +1,11 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.dao.RoleDao;
 import ru.kata.spring.boot_security.demo.entity.Role;
 
 import java.util.List;
 
-@Service
-@Transactional
-public class RoleService {
-    @Autowired
-    private RoleDao roleDao;
+public interface RoleService {
+    Role getRoleById(Long id);
 
-    public Role getRoleById(Long id) {
-        return roleDao.getRoleById(id);
-    }
-    public List<Role> getRolesList() {
-        return roleDao.getRolesList();
-    }
+    List<Role> getRolesList();
 }
